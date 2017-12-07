52f827832e27b595726ead611325a8a59cee2404
zouyuanheng <yuan_heng0455@126.com>
2017-12-07 18:02:06 +0800

    db_fixture

--- guest/settings.py
+++ guest/settings.py
@@ -75,7 +75,7 @@ WSGI_APPLICATION = 'guest.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
-
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -88,6 +88,20 @@ DATABASES = {
         'PASSWORD': '',
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
+    }
+}
+'''
+DATABASES={
+    'default':{
+        'ENGINE':'django.db.backends.mysql',
+        'HOST':'127.0.0.1',
+        'PORT':'3306',
+        'NAME':'guest_test',
+        'USER':'root',
+        'PASSWORD':'',
+        'OPTIONS':{
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'",
         },
     }
 }
