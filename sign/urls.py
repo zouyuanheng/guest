5f8ca76dedac8a1469af1297cb22226f0492d468
zouyuanheng <yuan_heng0455@126.com>
2017-12-14 16:21:37 +0800

    2017.12.14
    sec

--- sign/urls.py
+++ sign/urls.py
@@ -16,4 +16,7 @@ urlpatterns=[
     #security interface:
     #ex:/api/sec_get_event_list/
     url(r'^sec_get_event_list/',views_if_sec.get_event_list,name='get_event_list'),
+    #security interface:
+    #ex: /api/sec_add_event/
+    url(r'^sec_add_event/',views_if_sec.add_event,name='add_event'),
 ]
